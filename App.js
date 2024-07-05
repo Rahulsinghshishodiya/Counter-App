81fa1e9ee20b1f204d9322e6619773b17322c168
Rahulsinghshishodiya <147344794+Rahulsinghshishodiya@users.noreply.github.com>
2024-07-05 10:35:38 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import CheckBox from "./Components/CheckBox";
+import Searchbox from "./Components/Searchbox";
+
+function App() {
+  return (
+    <div className="App">
+      {/* <CheckBox /> */}
+      <Searchbox />
+    </div>
+  );
+}
+
+export default App;
